d3a47b097d7af197db122eccde62de1d6bb42364
thk7964 <yjs7894@gmail.com>
2025-12-04 09:22:30 +0900

    [level 1] Title: 2016년, Time: 0.03 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        int[] days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        int tot=0;
+        
+        for(int i= 0; i<(a-1); i++){
+            tot+= days[i];
+        }
+        tot+= b;
+        
+        String[] week ={"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        
+        answer =week[(tot-1)%7];
+        return answer;
+    }
+}
