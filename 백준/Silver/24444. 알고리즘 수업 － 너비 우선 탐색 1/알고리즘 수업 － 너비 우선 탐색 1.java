cf1fe1fb42277346a1979fb94de0f59f7cadef7a
thk7964 <yjs7894@gmail.com>
2026-01-25 23:59:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1488 ms, Memory: 97056 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,68 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int[] order;   // 방문 순서 저장
+    static int cnt = 1;   // 방문 번호
+
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+
+        visited[start] = true;
+        order[start] = cnt++;
+        q.add(start);
+
+        while (!q.isEmpty()) {
+            int x = q.poll();
+
+            for (int next : graph[x]) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    order[next] = cnt++;
+                    q.add(next);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken()); 
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken()); 
+
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+        order = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        bfs(r);
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(order[i]);
+        }
+    }
+}
